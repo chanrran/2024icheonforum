81c6f24f814d987f24f6dd72ede4697bc54ad57c
Youngchan Jo <35386378+chanrran@users.noreply.github.com>
2024-07-18 15:10:32 +0900

    app.py

--- app.py
+++ app.py
@@ -4,17 +4,24 @@ from collections import Counter
 import re
 import matplotlib.pyplot as plt
 from wordcloud import WordCloud
-from matplotlib import rc
+from matplotlib import font_manager, rc
 
 # 기본 한글 폰트 설정
-rc('font', family='Arial')
+font_path = '/usr/share/fonts/truetype/nanum/NanumGothic.ttf'
+font_manager.fontManager.addfont(font_path)
+plt.rc('font', family='NanumGothic')
 
 # GitHub에서 CSV 파일 읽기
 url = 'https://raw.githubusercontent.com/chanrran/2024icheonforum/main/Caselist_240718.csv'
 df = pd.read_csv(url)
 
+# 최상단 메시지 노출
+st.write("""
+    ## 이 서비스는 생성형 AI 공모전의 사례제출 현황을 분석하고 인사이트를 제공하기 위해 제작되었습니다.
+""")
+
 # 데이터 요약
-st.write(f"총 {df.shape[0]}건의 데이터가 확인되었습니다.")
+st.write(f"데이터를 통해 총 {df.shape[0]}건의 사례가 확인되었습니다.")
 
 # 옵션 선택 라디오 버튼 (오른쪽)
 col1, col2 = st.columns([3, 1])
@@ -38,6 +45,7 @@ with col1:
 
     selected_button = st.session_state.get('selected_button', None)
 
+    # 버튼 클릭 상태 유지 및 스타일 변경
     if selected_button == "company":
         company_counts = df['Company'].value_counts().sort_values(ascending=False)
         if display_option == "표":
@@ -72,7 +80,7 @@ with col1:
             st.write(level_counts)
         elif display_option == "그래프":
             fig, ax = plt.subplots()
-            ax.bar(level_counts.index, level_counts.values)
+            ax.barh(level_counts.index, level_counts.values)
             ax.set_xlabel('수준')
             ax.set_ylabel('건수')
             ax.set_title('수준별 건수')
@@ -89,7 +97,7 @@ if st.button("주요 키워드 추출"):
 # Wordcloud 만들기
 if st.button("Wordcloud 만들기"):
     text = ' '.join(df['Title'].dropna().astype(str))
-    wordcloud = WordCloud(width=800, height=400, background_color='white').generate(text)
+    wordcloud = WordCloud(width=800, height=400, background_color='white', font_path=font_path).generate(text)
     fig, ax = plt.subplots()
     ax.imshow(wordcloud, interpolation='bilinear')
     ax.axis('off')
