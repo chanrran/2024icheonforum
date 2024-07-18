9f8f2992bc13dcc89a3ff84b627e6af5268e94a7
Youngchan Jo <35386378+chanrran@users.noreply.github.com>
2024-07-18 14:28:08 +0900

    app.py

--- app.py
+++ app.py
@@ -1,18 +1,18 @@
 import pandas as pd
 import streamlit as st
-from io import BytesIO
+from io import StringIO
 from collections import Counter
 import re
 
 # 파일 업로드
-uploaded = st.file_uploader("파일을 업로드하세요", type=["xlsx"])
+uploaded = st.file_uploader("파일을 업로드하세요", type=["csv"])
 
 if uploaded is not None:
-    # 엑셀 파일 로드
-    df = pd.read_excel(BytesIO(uploaded.read()))
+    # CSV 파일 로드
+    df = pd.read_csv(uploaded)
 
     # 데이터프레임 열 이름 출력
-    st.write("엑셀 파일의 열 이름:", df.columns.tolist())
+    st.write("CSV 파일의 열 이름:", df.columns.tolist())
 
     # 필요한 열이 있는지 확인
     if 'Company' in df.columns and 'Title' in df.columns and 'Category' in df.columns and 'Level' in df.columns:
@@ -46,4 +46,4 @@ if uploaded is not None:
         st.subheader('카테고리별 키워드 분석')
         st.write(pd.DataFrame(top_keywords, columns=['키워드', '빈도수']))
     else:
-        st.error("엑셀 파일에 필요한 열(Company, Title, Category, Level)이 없습니다.")
+        st.error("CSV 파일에 필요한 열(Company, Title, Category, Level)이 없습니다.")
